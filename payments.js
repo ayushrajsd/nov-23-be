0c725f38b9704de6bed6333c79234c885d665c58
Scaler <scaler@Scalers-MacBook-Pro.local>
2023-12-15 12:46:01 +0530

    lecture 9 - payments

---
+++ payments.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const Razorpay = require("razorpay");
+const shortid = require("shortid");
+const cors = require("cors");
+
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+var instance = new Razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_SECRET_KEY,
+});
+
+app.post('/create-order', async (req, res) => {
+    // creating order id
+    // saving it in database
+    // return order id
+});
+
+app.post("/checkout", (req, res) => {
+  /**
+   * orderDetails = getOrderDetails(orderId)
+   */
+//   const orderId = req.body.orderId;
+// make call to databse to get order details
+  var options = {
+    amount: 10000, // amount in the smallest currency unit
+    currency: "INR",
+    receipt: shortid.generate(),// order id
+  };
+  instance.orders.create(options, function (err, order) {
+    console.log(order);
+    res.status(200).json({
+        message: "Order created successfully",
+        order: order
+    });
+  });
+});
+
+app.post("/verify",(req,res)=>{ 
+    try{
+        console.log("web hook called")
+        res.json({status:"ok"})
+        //communication with database
+
+    }catch(err){
+        console.log(err)
+    }
+})
+
+app.listen(3000, () => {
+  console.log("Server is running at port 3000");
+});
